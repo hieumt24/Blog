473c2461402ed2045874cfddcbb7bcc4de0cf2ee
Hieu Mai Trong <hieu.maitrong@nashtechglobal.com>
2025-01-09 17:31:56 +0700

    feaute : logout

--- blog-admin-ui/src/app/app.module.ts
+++ blog-admin-ui/src/app/app.module.ts
@@ -43,6 +43,7 @@ import { ToastModule } from 'primeng/toast';
 import { MessageService } from 'primeng/api';
 import { AlertService } from './shared/services/alert.service';
 import { HttpClientModule } from '@angular/common/http';
+import { TokenStorageService } from './shared/services/token-storage.service';
 const APP_CONTAINERS = [
   DefaultFooterComponent,
   DefaultHeaderComponent,
@@ -91,7 +92,8 @@ const APP_CONTAINERS = [
     Title,
     MessageService,
     AlertService,
-    AdminApiAuthApiClient
+    AdminApiAuthApiClient,
+    TokenStorageService
   ],
   bootstrap: [AppComponent]
 })
