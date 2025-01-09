eb4dfec0edb6df51ff5be8d7066e044d4a2de7a4
Mai Trong Hieu <90890367+hieumt24@users.noreply.github.com>
2025-01-09 17:37:12 +0700

    Feature login (#11)
    * authen and authorize in fe
    
    * alert message
    
    * login with angular
    
    * feaute : logout
    
    * update code

--- blog-admin-ui/src/app/views/auth/login/login.component.ts
+++ blog-admin-ui/src/app/views/auth/login/login.component.ts
@@ -1,22 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AdminApiAuthApiClient, AuthenticatedResult, LoginRequest } from 'src/app/api/admin-api.service.generated';
 import { AlertService } from 'src/app/shared/services/alert.service';
+import {UrlConstants} from "src/app/shared/constants/url.constants";
+import { TokenStorageService } from 'src/app/shared/services/token-storage.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnDestroy {
   loginForm : FormGroup;
-
+  private ngUnsubscribe = new Subject<void>();
+  loading = false;
   constructor(
     private fb : FormBuilder, 
     private authApiClient: AdminApiAuthApiClient,
     private alertService: AlertService,
-    private router : Router
+    private router : Router,
+    private tokenService : TokenStorageService
     ) {
     this.loginForm = this.fb.group({
       userName : new FormControl('', Validators.required),
@@ -24,22 +29,33 @@ export class LoginComponent {
    })
   }
 
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
+
   login() {
+    this.loading = true;
     var request : LoginRequest = new LoginRequest({
       userName : this.loginForm.controls['userName'].value,
       password : this.loginForm.controls['password'].value
     });
 
-    this.authApiClient.login(request).subscribe({
+    this.authApiClient.login(request)
+    .pipe(takeUntil(this.ngUnsubscribe))
+    .subscribe({
       next:(res:AuthenticatedResult) => {
           //save token and refresh token to local storage
-
+          this.tokenService.saveToken(res.token);
+          this.tokenService.saveRefreshToken(res.refreshToken);
+          this.tokenService.saveUser(res);
           //redirect to dashboard
-          this.router.navigate(['/dashboard']);
+          this.router.navigate([UrlConstants.HOME]);
       },
       error: (error: any) => {
         console.log(error);
-        this.alertService.showError('Login failed');
+        this.alertService.showError('Tài khoản hoặc mật khẩu không đúng');
+        this.loading = false;
       },
     })
   }
